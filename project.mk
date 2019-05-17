7bcfd7f35d68b28342e0db73cc133b0ab342ca76
Rafael Azevedo <mail@rafaelazevedo.me>
2019-05-17 13:03:46 -0400

    Fix for PR

---
+++ project.mk
@@ -0,0 +1,6 @@
+#Project specific values
+YAML_DIRECTORY?=deploy
+SELECTOR_SYNC_SET_TEMPLATE?=scripts/templates/selectorsyncset.yaml
+GIT_ROOT?=$(shell git rev-parse --show-toplevel 2>&1)
+SELECTOR_SYNC_SET_DESTINATION?=${GIT_ROOT}/00-osd-managed-prometheus-exporter-ebs-iops-reporter.selectorsyncset.yaml
+GIT_HASH?=$(shell git rev-parse --short=7 HEAD 2>&1)
